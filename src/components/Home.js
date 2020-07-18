8127aa4f47c26d9503a72d7c11aef164a0bb3b8e
Justin Cook <justin.cook.dev@gmail.com>
2020-07-18 11:03:16 -0700

    basic routing setup

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,50 +1,17 @@
-import React from 'react'
+import React from 'react';
 import {
   Container,
   Divider,
-  Dropdown,
-  Grid,
   Header,
   Image,
   List,
-  Menu,
   Segment,
-} from 'semantic-ui-react'
+} from 'semantic-ui-react';
 
 
 
 const HomepageLayout = () => (
   <div>
-    <Menu fixed='top' style={{
-        backgroundColor: 'yellow',}}> 
-      <Container style={{
-        backgroundColor: 'yellow',}}>
-        <Menu.Item as='a' header
-        style ={{
-          backgroundColor: "orange"}
-        
-
-        }>
-          <Image size='mini' src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRFG6E90BJmHhB7JaUXg1XaWpQmxA8NGLNJqA&usqp=CAU' style={{ marginRight: '1.5em' }} />
-          Cheese Wizards
-        </Menu.Item>
-        <Menu.Item as='a'>Home</Menu.Item>
-
-        <Dropdown
-        style ={{
-
-          backgroundColor: "orange"
-        }} 
-         item simple text='MENU'>
-          <Dropdown.Menu>
-            <Dropdown.Item>Products</Dropdown.Item>
-            <Dropdown.Item>User Profile</Dropdown.Item>
-            <Dropdown.Item>Cart</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      </Container>
-    </Menu>
-
     <Container text style={{ marginTop: '7em' }}>
       <Header as='h1'>WELCOME TO CHEESE WIZARDS</Header>
       <Image size='large' src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRoQDJUuzhE5MneFhgbU5Ij9gFcOCIF14Py2g&usqp=CAU'/>
@@ -54,7 +21,7 @@ const HomepageLayout = () => (
       </p>
     </Container>
 
-    <Segment 
+    <Segment
         vertical style={{  backgroundColor: "yellow" , margin: '5em 0em 0em', padding: '5em 0em' }}>
       <Container  style={{
         backgroundColor: 'orange',}}
@@ -79,7 +46,7 @@ const HomepageLayout = () => (
       </Container>
     </Segment>
   </div>
-)
+);
 
 
-export default HomepageLayout
+export default HomepageLayout;
--- src/components/index.js
+++ src/components/index.js
@@ -4,4 +4,5 @@ export { default as Products } from './Products';
 export { default as Order } from './Order';
 export { default as ProductDetails } from './productDetails';
 export {default as HomepageLayout} from './Home';
+export {default as Nav} from './Nav';
 
